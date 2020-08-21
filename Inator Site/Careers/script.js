4019f6b8fa20e959a31e80264ae3cb174755c3bd
Abhiraj Mhetre <55054832+AbhirajMhetre@users.noreply.github.com>
2020-08-21 17:59:24 +0530

    Add files via upload

--- Inator Site/Careers/script.js
+++ Inator Site/Careers/script.js
@@ -27,6 +27,22 @@ function closeNav() {
 }
 
 
+//Get the button
+var mybutton = document.getElementById("applyBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+
 
 //   AOS EFFECTS
 AOS.init({
