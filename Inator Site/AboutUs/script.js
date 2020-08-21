0683343a29e87f0a81af4d70573a991c90eced6b
Abhiraj Mhetre <55054832+AbhirajMhetre@users.noreply.github.com>
2020-08-21 17:58:05 +0530

    Add files via upload

--- Inator Site/AboutUs/script.js
+++ Inator Site/AboutUs/script.js
@@ -144,6 +144,22 @@ anime.timeline({loop: true})
 
 
 
+// child organisations
+
+$l = $(".left");
+$r = $(".right");
+
+$l.mouseenter(function () {
+  $(".container").addClass("left-is-hovered");
+}).mouseleave(function () {
+  $(".container").removeClass("left-is-hovered");
+});
+
+$r.mouseenter(function () {
+  $(".container").addClass("right-is-hovered");
+}).mouseleave(function () {
+  $(".container").removeClass("right-is-hovered");
+});
 
 
   
