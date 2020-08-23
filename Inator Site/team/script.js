1cc460d05b2c5c30cdcb40bcfe3dcf1cefb55539
Abhiraj Mhetre <55054832+AbhirajMhetre@users.noreply.github.com>
2020-08-23 22:43:04 +0530

    Add files via upload

--- Inator Site/team/script.js
+++ Inator Site/team/script.js
@@ -25,3 +25,365 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 }
+
+
+
+//New teammates 
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger1").hover(
+    function (e) {
+      $("div#pop-up1").show();
+    },
+    function () {
+      $("div#pop-up1").hide();
+    }
+  );
+
+  $(".trigger1").mousemove(function (e) {
+    $("div#pop-up1")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger2").hover(
+    function (e) {
+      $("div#pop-up2").show();
+    },
+    function () {
+      $("div#pop-up2").hide();
+    }
+  );
+
+  $(".trigger2").mousemove(function (e) {
+    $("div#pop-up2")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger3").hover(
+    function (e) {
+      $("div#pop-up3").show();
+    },
+    function () {
+      $("div#pop-up3").hide();
+    }
+  );
+
+  $(".trigger3").mousemove(function (e) {
+    $("div#pop-up3")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger4").hover(
+    function (e) {
+      $("div#pop-up4").show();
+    },
+    function () {
+      $("div#pop-up4").hide();
+    }
+  );
+
+  $(".trigger4").mousemove(function (e) {
+    $("div#pop-up4")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger5").hover(
+    function (e) {
+      $("div#pop-up5").show();
+    },
+    function () {
+      $("div#pop-up5").hide();
+    }
+  );
+
+  $(".trigger5").mousemove(function (e) {
+    $("div#pop-up5")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger6").hover(
+    function (e) {
+      $("div#pop-up6").show();
+    },
+    function () {
+      $("div#pop-up6").hide();
+    }
+  );
+
+  $(".trigger6").mousemove(function (e) {
+    $("div#pop-up6")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger7").hover(
+    function (e) {
+      $("div#pop-up7").show();
+    },
+    function () {
+      $("div#pop-up7").hide();
+    }
+  );
+
+  $(".trigger7").mousemove(function (e) {
+    $("div#pop-up7")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger8").hover(
+    function (e) {
+      $("div#pop-up8").show();
+    },
+    function () {
+      $("div#pop-up8").hide();
+    }
+  );
+
+  $(".trigger8").mousemove(function (e) {
+    $("div#pop-up8")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger9").hover(
+    function (e) {
+      $("div#pop-up9").show();
+    },
+    function () {
+      $("div#pop-up9").hide();
+    }
+  );
+
+  $(".trigger9").mousemove(function (e) {
+    $("div#pop-up9")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger10").hover(
+    function (e) {
+      $("div#pop-up10").show();
+    },
+    function () {
+      $("div#pop-up10").hide();
+    }
+  );
+
+  $(".trigger10").mousemove(function (e) {
+    $("div#pop-up10")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger11").hover(
+    function (e) {
+      $("div#pop-up11").show();
+    },
+    function () {
+      $("div#pop-up11").hide();
+    }
+  );
+
+  $(".trigger11").mousemove(function (e) {
+    $("div#pop-up11")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger12").hover(
+    function (e) {
+      $("div#pop-up12").show();
+    },
+    function () {
+      $("div#pop-up12").hide();
+    }
+  );
+
+  $(".trigger12").mousemove(function (e) {
+    $("div#pop-up12")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger13").hover(
+    function (e) {
+      $("div#pop-up13").show();
+    },
+    function () {
+      $("div#pop-up13").hide();
+    }
+  );
+
+  $(".trigger13").mousemove(function (e) {
+    $("div#pop-up13")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger14").hover(
+    function (e) {
+      $("div#pop-up14").show();
+    },
+    function () {
+      $("div#pop-up14").hide();
+    }
+  );
+
+  $(".trigger14").mousemove(function (e) {
+    $("div#pop-up14")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger15").hover(
+    function (e) {
+      $("div#pop-up15").show();
+    },
+    function () {
+      $("div#pop-up15").hide();
+    }
+  );
+
+  $(".trigger15").mousemove(function (e) {
+    $("div#pop-up15")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger16").hover(
+    function (e) {
+      $("div#pop-up16").show();
+    },
+    function () {
+      $("div#pop-up16").hide();
+    }
+  );
+
+  $(".trigger16").mousemove(function (e) {
+    $("div#pop-up16")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
+$(function () {
+  var moveLeft = 20;
+  var moveDown = 10;
+
+  $(".trigger17").hover(
+    function (e) {
+      $("div#pop-up17").show();
+    },
+    function () {
+      $("div#pop-up17").hide();
+    }
+  );
+
+  $(".trigger17").mousemove(function (e) {
+    $("div#pop-up17")
+      .css("top", e.pageY + moveDown)
+      .css("left", e.pageX + moveLeft);
+  });
+});
+
+
